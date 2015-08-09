5ed089213b8cf28510f2e5611aab26150e784ed1
RebootJeff <RebootJeff@users.noreply.github.com>
2015-08-09 02:33:08 -0700

    Spruce up the frontend

--- client/app.js
+++ client/app.js
@@ -1,100 +1,78 @@
-function ready(fn) {
-  'use strict';
-  if(document.readyState !== 'loading') {
-    fn();
+'use strict';
+
+// Internal dependencies
+var Utils = require('./utils');
+
+var input = document.querySelector('input');
+var overlay = document.querySelector('.overlay');
+var tableHead = document.querySelector('thead');
+var tableBody = document.querySelector('tbody');
+
+Utils.preventDefaultFormBehavior();
+
+window.getPullRequests = function() {
+  var username = input.value;
+
+  if(username !== '') {
+    toggleSpinner(overlay);
+    getData(username);
+  }
+
+  return false;
+};
+
+function getData(username) {
+  var url = '/api/' + username;
+  var request = new XMLHttpRequest();
+
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    toggleSpinner(overlay);
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      tableHead.classList.remove('hide');
+      renderPullRequests(data);
+    } else {
+      // We reached our target server, but it returned an error
+    }
+  };
+  request.onerror = function() {
+    // There was a connection error of some sort
+    toggleSpinner(overlay);
+  };
+
+  request.send();
+}
+
+function toggleSpinner(overlay) {
+  if(overlay.classList.contains('hide')) {
+    overlay.classList.remove('fadeOut');
+    overlay.classList.remove('hide');
+    overlay.classList.add('fadeIn');
   } else {
-    document.addEventListener('DOMContentLoaded', fn);
+    overlay.classList.remove('fadeIn');
+    overlay.classList.add('fadeOut');
+    setTimeout(function() {
+      overlay.classList.add('hide');
+    }, 1000); // delay must be equal to fadeOut CSS animation duration
   }
 }
 
-ready(function() {
-  'use strict';
+function renderPullRequests(pullRequests) {
+  var newHtml = pullRequests.map(makePullRequestHtml)
+    .join('');
+  tableBody.innerHTML = newHtml;
+}
 
-  var input = document.querySelector('input');
-  var overlay = document.querySelector('.overlay');
-  var tableBody = document.querySelector('tbody');
-
-  document.querySelector('form').addEventListener('submit', function(event){
-    event.preventDefault();
-  }, true);
-
-  window.getPullRequests = function() {
-    var username = input.value;
-
-    if(username !== '') {
-      toggleSpinner(overlay);
-      getData(username);
-    }
-    return false;
-  };
-
-  function getData(username) {
-    var url = '/api/' + username;
-    var request = new XMLHttpRequest();
-
-    request.open('GET', url, true);
-
-    request.onload = function() {
-      toggleSpinner(overlay);
-      if (request.status >= 200 && request.status < 400) {
-        var data = JSON.parse(request.responseText);
-        renderPullRequests(data);
-      } else {
-        // We reached our target server, but it returned an error
-      }
-    };
-    request.onerror = function() {
-      // There was a connection error of some sort
-      toggleSpinner(overlay);
-    };
-
-    request.send();
-  }
-
-  function toggleSpinner(overlay) {
-    if(overlay.classList.contains('hide')) {
-      overlay.classList.remove('fadeOut');
-      overlay.classList.remove('hide');
-      overlay.classList.add('fadeIn');
-    } else {
-      overlay.classList.remove('fadeIn');
-      overlay.classList.add('fadeOut');
-      setTimeout(function() {
-        overlay.classList.add('hide');
-      }, 1000); // delay must be equal to fadeOut CSS animation duration
-    }
-  }
-
-  function renderPullRequests(pullRequests) {
-    var newHtml = pullRequests.map(makePullRequestHtml)
-      .join('');
-    tableBody.innerHTML = newHtml;
-  }
-
-  function makePullRequestHtml(pullRequest) {
-    return '<tr>' +
-      '<td><img class="avatar" src="' + pullRequest.parentAvatar + '"></td>' +
-      '<td><a href="' + pullRequest.parentUrl + '">' + pullRequest.parentName + '</a></td>' +
-      '<td>' + pullRequest.title + '</td>' +
-      '<td><a href="' + pullRequest.url + '">' + (pullRequest.merged ? 'merged' : pullRequest.state) + '</a></td>' +
-      '<td>' + createLatestDateString(pullRequest) + '</td>' +
-      // '<td>' + pullRequest.foo + '</td>' +
-      '</tr>';
-  }
-
-  function createLatestDateString(pullRequest) {
-    var timestamps = [
-      new Date(pullRequest.createdAt),
-      new Date(pullRequest.modifiedAt),
-      new Date(pullRequest.mergedAt),
-      new Date(pullRequest.closedAt)
-    ];
-    timestamps = timestamps.filter(function(timestamp) {
-      return typeof timestamp === 'number';
-    });
-    var latestDate = Math.max.apply(Math, timestamps);
-    latestDate = new Date(latestDate);
-    return latestDate.toLocaleDateString();
-  }
-
-});
+function makePullRequestHtml(pullRequest) {
+  return '<tr>' +
+    '<td><img class="avatar" src="' + pullRequest.parentAvatar + '"></td>' +
+    '<td><a href="' + pullRequest.parentUrl + '">' + pullRequest.parentName + '</a></td>' +
+    '<td>' + pullRequest.title + '</td>' +
+    '<td class="status">' + (pullRequest.merged ? '<span class="merged">merged</span>' : pullRequest.state) + '</td>' +
+    '<td class="date"><a href="' + pullRequest.url + '">' + Utils.getLatestDateString(pullRequest) + '</a></td>' +
+    '<td class="adds-dels">+' + pullRequest.additions + ' -' + pullRequest.deletions + '</td>' +
+    // '<td>' + pullRequest.foo + '</td>' +
+    '</tr>';
+}
---
+++ client/utils.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// External dependencies
+var R = require('ramda');
+
+var Utils = {};
+
+Utils.preventDefaultFormBehavior = function() {
+  document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault();
+  }, true);
+};
+
+var isNotNil = R.compose(R.not, R.isNil);
+var pickTimestamps = R.compose(
+  R.filter(isNotNil),
+  R.values,
+  R.pick(['createdAt', 'modifiedAt', 'mergedAt', 'closedAt'])
+);
+function convertTimestampToDate(timestamp) {
+  return new Date(timestamp);
+}
+
+var convertTimestampsToDates = R.map(convertTimestampToDate);
+
+Utils.getLatestDateString = function(pullRequest) {
+  var timestamps = pickTimestamps(pullRequest);
+  var dates = convertTimestampsToDates(timestamps);
+  var latestDate = Math.max.apply(Math, dates);
+  // Math.max will yield UNIX timestamp (integer), so we must re-convert.
+  latestDate = convertTimestampToDate(latestDate);
+  return latestDate.toLocaleDateString();
+};
+
+module.exports = Utils;
